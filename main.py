0635f8e94942806b217ca125854326bed4362ead
leeyanhuilyh <leeyanhuilyh@gmail.com>
2025-07-24 00:56:06 +0800

    DOC: Add rough outline of app

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+# Reading inputs
+# 1) name of car
+# 2) x pos, y pos, direction (only n, e, s, w)
+# 3) enter commands for car
+#   3a) eg. FFRFFFFRRL
+#   This means car A will move forward twice, turn right, move forward four times, turn right twice, and turn left once.
+# 4) print out current list of cars with name, position, direction, commands
+#   4a) eg. - A, (1,2) N, FFRFFFFRRL
+# 5) Give user choice to add 1 more car, or run simulation
+
+
+# create field of w x h (width is column, height is row)
+
+# Add car with name, position, direction
+# make a class for car
+
+# Run simulation
+# 1) at each step, advance all cars by 1 command
+# 2) if there is a collision, both cars collide at THAT square
+
