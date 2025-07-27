22d12ea199285fdc3cfd5bf6bae2f7f2c6a1f3b4
leeyanhuilyh <leeyanhuilyh@gmail.com>
2025-07-27 12:38:10 +0800

    TEST: Add test for car turning

--- tests/test_car.py
+++ tests/test_car.py
@@ -21,3 +21,18 @@ def test_car_creation(name, x, y, direction):
 def test_invalid_car_creation(name, x, y, direction):
     with pytest.raises(AssertionError):
         Car(name, x, y, direction)
+
+@pytest.mark.parametrize(
+    "start_direction, turns, expected_directions",
+    [
+        ("N", ["L"], ["W"]),
+        ("E", ["L", "L"], ["N", "W"]),
+        ("S", ["R", "R"], ["W", "N"]),
+        ("W", ["L", "R", "R"], ["S", "W", "N"]),
+    ]
+)
+def test_car_turn(start_direction, turns, expected_directions):
+    car = Car("A", 0, 0, start_direction)
+    for turn, expected in zip(turns, expected_directions):
+        car.turn(turn)
+        assert car.direction == expected
