94497923dddae20cdb2880969bae1d2fab99c5e2
leeyanhuilyh <leeyanhuilyh@gmail.com>
2025-07-29 00:43:19 +0800

    FEAT: Make debug cases use menu variable

--- main.py
+++ main.py
@@ -5,82 +5,48 @@ from field import Field
 from simulator import Simulator
 
 def main():
-    simulator = Simulator()
+    menu = Menu()
     print("Welcome to Auto Driving Car Simulation!")
     print()
     field_size = input("Please enter the width and height of the simulation field in x y format: ")
     if field_size == 'd1':
         print("Debug case 1")
-        field = Field(10, 10)
-        simulator.set_field(field)
-        car = Car("A", 1, 2, "N")
-        car.add_command("FFRFFFFRRL")
-        simulator.add_car(car)
+        field_size = "10 10"
+        menu.set_field(field_size)
+        menu.add_car("A", (1, 2, "N"), "FFRFFFFRRL")
     elif field_size == 'd2':
         print("Debug case 2")
-        field = Field(10, 10)
-        simulator.set_field(field)
-        car = Car("A", 1, 2, "N")
-        car.add_command("FFRFFFFRRL")
-        simulator.add_car(car)
-        car = Car("B", 7, 8, "W")
-        car.add_command("FFLFFFFFFF")
-        simulator.add_car(car)
+        field_size = "10 10"
+        menu.set_field(field_size)
+        menu.add_car("A", (1, 2, "N"), "FFRFFFFRRL")
+        menu.add_car("B", (7, 8, "W"), "FFLFFFFFFF")
     elif field_size == 'd3':
         print("Debug case 3")
-        field = Field(10, 10)
-        simulator.set_field(field)
-        car = Car("A", 1, 2, "N")
-        car.add_command("FFRFFFFRRL")
-        simulator.add_car(car)
-        car = Car("B", 7, 8, "W")
-        car.add_command("FFLFFFFFFF")
-        simulator.add_car(car)
-        car = Car("C", 5, 5, "N")
-        car.add_command("RRFF")
-        simulator.add_car(car)
+        field_size = "10 10"
+        menu.set_field(field_size)
+        menu.add_car("A", (1, 2, "N"), "FFRFFFFRRL")
+        menu.add_car("B", (7, 8, "W"), "FFLFFFFFFF")
+        menu.add_car("C", (5, 5, "N"), "RRFF")
     else:
-        assert len(field_size.split()) == 2, "Field size must be in x y format"
-        field_size = field_size.split()
-        assert int(field_size[0]) > 0, "Width must be positive"
-        assert int(field_size[1]) > 0, "Height must be positive"
-        width, height = int(field_size[0]), int(field_size[1])
-
-        # create field of w x h (width is column, height is row)
-        field = Field(width, height)
-        simulator.set_field(field)
-        print(f"You have created a field of {width} x {height}.")
-        print()
-
-        mode = -1
-        while mode != "2":
+        menu.set_field(field_size)
+        while menu.mode != "2":
             mode = input("""\
 Please choose from the following options: 
 [1] Add a car to field
 [2] Run simulation
 """)
-            assert mode in ["1", "2"], "Invalid mode"
-            if mode == "1":
+            menu.set_mode(mode)
+            if menu.mode == "1":
                 car_name = input("Please enter the name of the car: ")
-                assert 0 < len(car_name) < 64, "Car name must be non-empty and less than 64 characters"
+                menu.get_car_name(car_name)
                 car_position = input(f"Please enter the position of the car {car_name} in x y Direction format: ")
-                assert len(car_position.split()) == 3, "Car position must be in x y Direction format"
-                car_position = car_position.split()
-                assert int(car_position[0]) >= 0 and int(car_position[0]) < width, "Car position must be within the field"
-                assert int(car_position[1]) >= 0 and int(car_position[1]) < height, "Car position must be within the field"
-                assert car_position[2].upper() in ["N", "E", "S", "W"], "Invalid direction. Must be N, E, S, or W (case insensitive)."
-                car_direction = car_position[2].upper()
-                car_position = (int(car_position[0]), int(car_position[1]))
+                car_position = menu.get_car_position_direction(car_position)
+
                 car_commands = input(f"Please enter the commands for the car {car_name}: ")
-                assert len(car_commands) < 128, "Car commands must be less than 128 characters"
-                car_commands = car_commands.upper()
-                assert all(command in ["F", "L", "R"] for command in car_commands), "Invalid command. Must be f, b, l, or r (case insensitive)."
+                car_commands = menu.get_car_commands(car_commands)
+
                 print("Your current list of cars are:")
-                car = Car(car_name, car_position[0], car_position[1], car_direction)
-                car.add_command(car_commands)
-                simulator.add_car(car)
-                print(simulator.car_print)
-                print()
+                menu.add_car(car_name, car_position, car_commands)
  
     # Reading inputs
     # 1) name of car
@@ -94,6 +60,7 @@ Please choose from the following options:
 
     # Run simulation
     # 1) at each step, advance all cars by 1 command
+    simulator = menu.simulator
     step = 1
     while len(simulator.cars):
         curr_positions = {}
--- menu.py
+++ menu.py
@@ -1,10 +1,54 @@
 from car import Car
+from simulator import Simulator
+from field import Field
 
 class Menu:
     def __init__(self):
-        self.cars = []
-        self.car_print = ""
+        self.simulator = Simulator()
+        self.mode = -1
 
-    def add_car(self, car: Car):
-        self.cars.append(car)
-        self.car_print += f"- {car.name}, ({car.x}, {car.y}) {car.direction}, {''.join(list(car.commands))}\n"
+    def set_field(self, field_input: str):
+        field_size = field_input.split()
+        assert len(field_size) == 2, "Field size must be in x y format"
+        assert int(field_size[0]) > 0, "Width must be positive"
+        assert int(field_size[1]) > 0, "Height must be positive"
+        width, height = int(field_size[0]), int(field_size[1])
+        field = Field(width, height)
+        self.simulator.set_field(field)
+        print(f"You have created a field of {width} x {height}.")
+        print()
+
+    def set_mode(self, mode: str):
+        assert mode in ["1", "2"], "Invalid mode"
+        self.mode = mode
+
+    def add_car(self, car_name: str, car_position: tuple, car_commands: str):
+        car = Car(car_name, car_position[0], car_position[1], car_position[2])
+        car.add_command(car_commands)
+        self.simulator.add_car(car)
+        print(self.simulator.car_print)
+
+    def get_car_name(self, car_name: str):
+        assert 0 < len(car_name) < 64, "Car name must be non-empty and less than 64 characters"
+        return car_name
+
+    def get_car_position_direction(self, car_position: str):
+        assert len(car_position.split()) == 3, "Car position must be in x y Direction format"
+        car_position = car_position.split()
+        assert int(car_position[0]) >= 0 and int(car_position[0]) < self.simulator.field.width, "Car position must be within the field"
+        assert int(car_position[1]) >= 0 and int(car_position[1]) < self.simulator.field.height, "Car position must be within the field"
+        assert car_position[2].upper() in ["N", "E", "S", "W"], "Invalid direction. Must be N, E, S, or W (case insensitive)."
+        direction = self.get_car_direction(car_position[2])
+        return (int(car_position[0]), int(car_position[1]), direction)
+
+    def get_car_direction(self, car_direction: str):
+        assert car_direction.upper() in ["N", "E", "S", "W"], "Invalid direction. Must be N, E, S, or W (case insensitive)."
+        return car_direction.upper()
+
+    def get_car_commands(self, car_commands: str):
+        assert len(car_commands) < 128, "Car commands must be less than 128 characters"
+        car_commands = car_commands.upper()
+        assert all(command in ["F", "L", "R"] for command in car_commands), "Invalid command. Must be f, b, l, or r (case insensitive)."
+        return car_commands
+
+
